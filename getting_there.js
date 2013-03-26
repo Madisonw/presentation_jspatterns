269d683cbabed02b7d1750939425e9ceab575da7
Madison Williams <madisonjw@gmail.com>
2013-03-26 13:53:34 -0700

    added getting_there.js

---
+++ getting_there.js
@@ -0,0 +1,34 @@
+
+
+
+var NAMESPACE = (function(){
+    var varname = null;
+    var varname1 = null;
+    var varname2 = null;
+    var varname3 = null;
+    var varname4 = null;
+    var varname5 = null;
+    var varname6 = null;
+    var varname7 = null;
+    var varname8 = null;
+    var varname9 = null;
+    var varname10 = null;
+    var varname11 = null;
+
+
+
+    function method1() {
+
+    }
+
+    function method2() {
+
+    }
+
+
+    return {
+        method1 : method1,
+        method2 : method2
+    }
+
+}())
